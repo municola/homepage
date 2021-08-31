54f0a8af80c4fb11a044fd045bb1a165cd4d47bd
municola <municola@student.ethz.ch>
2021-08-31 19:50:47 +0200

    trigger build, removed useless line

--- src/components/Navbar/Index.js
+++ src/components/Navbar/Index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import {FaBars} from 'react-icons/fa'
 import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks,
 	NavBtn, NavBtnLink} from './NavBarElements'
